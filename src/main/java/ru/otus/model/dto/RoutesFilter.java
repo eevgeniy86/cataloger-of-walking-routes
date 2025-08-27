d3b389d02be285f15f1b1f87722800532787d75c
eevgeniy86 <eelistratov1986@yandex.ru>
2025-08-27 18:56:03 +0300

    modified:   RouteRestController

---
+++ src/main/java/ru/otus/model/dto/RoutesFilter.java
@@ -0,0 +1,3 @@
+package ru.otus.model.dto;
+
+public record RoutesFilter() {}
--- src/main/java/ru/otus/processors/UrlProcessor.java
+++ src/main/java/ru/otus/processors/UrlProcessor.java
@@ -1,8 +1,7 @@
 package ru.otus.processors;
 
-import ru.otus.exceptions.UrlConstructorException;
-
 import java.net.URL;
+import ru.otus.exceptions.UrlConstructorException;
 
 public interface UrlProcessor<T> {
     URL fromObjectToUrl(T obj) throws UrlConstructorException;
