a4ea6106d0b2a36034395398d9cc95b9da3f9de8
eevgeniy86 <eelistratov1986@yandex.ru>
2025-08-29 14:50:24 +0300

    Minor

---
+++ src/main/java/ru/otus/controllers/RelationRestController.java
@@ -0,0 +1,22 @@
+package ru.otus.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.model.domain.Relation;
+import ru.otus.model.service.DBServiceRelation;
+
+@RestController
+@RequestMapping("${rest.api.prefix}${rest.api.version}")
+@AllArgsConstructor
+public class RelationRestController {
+
+    private final DBServiceRelation dbServiceRelation;
+
+    @Operation(summary = "Get relations by route_id")
+    @GetMapping("/route/{id}/relation")
+    public List<Relation> getRelationsByRouteId(@PathVariable(name = "id") long route_id) {
+        return dbServiceRelation.getRelationsByRouteId(route_id);
+    }
+}
--- src/main/java/ru/otus/model/service/DBServiceRelationImpl.java
+++ src/main/java/ru/otus/model/service/DBServiceRelationImpl.java
@@ -1,3 +1,6 @@
 package ru.otus.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DBServiceRelationImpl {}
